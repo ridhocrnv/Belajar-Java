bad338653c9683e9326cee31afff2b6319dcd841
Ridho Crnv <ridhoahmadirawan281@gmail.com>
2026-02-04 17:58:51 +0800

    add: Latihan/Latihan.java

---
+++ Java-Dasar/Latihan/Latihan1.java
@@ -0,0 +1,13 @@
+package Latihan;
+
+public class Latihan1 {
+    public static void main(String[] args) {
+        String name = "Ridho Ahmad Irawan";
+        int age = 20;
+        String address = "Jl. HEA Mokodompit";
+
+        System.out.println("Nama: " + name);
+        System.out.println("Usia: " + age);
+        System.out.println("Alamat: " + address);
+    }
+}
