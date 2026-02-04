40303bcfc3750a3128d22d9f82cad2e5534f94f9
Ridho Crnv <ridhoahmadirawan281@gmail.com>
2026-02-04 17:16:10 +0800

    add: TipeDataBoolean.java

---
+++ Java-Dasar/TipeDataBoolean.java
@@ -0,0 +1,10 @@
+public class TipeDataBoolean {
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+
+        System.out.print(a);
+        System.out.print(b);
+        
+    }
+}
