b98c1d8893da1850f02dec02b2971edd64311e8c
Ridho Crnv <ridhoahmadirawan281@gmail.com>
2026-02-04 17:24:33 +0800

    add TipeDataString.java

---
+++ Java-Dasar/TipeDataString.java
@@ -0,0 +1,9 @@
+public class TipeDataString {
+    public static void main(String[] args) {
+        String firstName = "Ridho Ahmad";
+        String lastName = "Irawan";
+
+        System.out.println(firstName + " " + lastName);
+        System.out.println(lastName);
+    }
+}
