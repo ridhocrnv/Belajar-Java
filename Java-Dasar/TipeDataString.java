0230e109712cb6e9d00eec780d3ae5b386c863fc
Ridho Crnv <ridhoahmadirawan281@gmail.com>
2026-02-04 17:31:17 +0800

    refactor: TipeDataString

--- Java-Dasar/TipeDataString.java
+++ Java-Dasar/TipeDataString.java
@@ -2,8 +2,10 @@ public class TipeDataString {
     public static void main(String[] args) {
         String firstName = "Ridho Ahmad";
         String lastName = "Irawan";
+        String fullName = firstName + " " + lastName;
 
-        System.out.println(firstName + " " + lastName);
+        System.out.println(firstName);
         System.out.println(lastName);
+        System.out.println(fullName);
     }
 }
