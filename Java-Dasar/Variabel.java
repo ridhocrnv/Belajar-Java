ca99d4f995406ba6a41523990a206629129905af
Ridho Crnv <ridhoahmadirawan281@gmail.com>
2026-03-27 20:45:57 +0800

    add: Variabel.java

---
+++ Java-Dasar/Variabel.java
@@ -0,0 +1,13 @@
+public class Variabel {
+    public static void main(String[] args) {
+        String name;
+        name = "Ridho Ahmad Irawan";
+
+        int age = 21;
+        String address = "Jl. HEA Mokodompit, Kel. Lalolara, Kec. Kambu";
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.print(address);
+    }
+}
