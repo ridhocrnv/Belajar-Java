019e3027d4ec6a1775cb230cb238ebfe918985cf
Ridho Crnv <ridhoahmadirawan281@gmail.com>
2026-04-18 13:52:29 +0800

    add: JavaStatement.java

---
+++ Java-Tutorial/JavaStatement.java
@@ -0,0 +1,7 @@
+public class JavaStatement {
+    public static void main(String[] args) {
+        System.out.println("Java is Fun!");
+        System.out.println("Have a good day!");
+        System.out.println("Learning Java is fun!");
+    }
+}
