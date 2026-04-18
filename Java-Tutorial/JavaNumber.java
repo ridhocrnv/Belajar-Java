a8509778a43004d9875f496496748bb5d4257246
Ridho Crnv <ridhoahmadirawan281@gmail.com>
2026-04-18 14:04:23 +0800

    add: multiline JavaNumber.java

--- Java-Tutorial/JavaNumber.java
+++ Java-Tutorial/JavaNumber.java
@@ -2,6 +2,8 @@ public class JavaNumber {
     public static void main(String[] args) {
         // This is a comment
         System.out.println(3);
+        /* This is
+        a multiline comment */
         System.out.println(3 + 3);
     }
 }
